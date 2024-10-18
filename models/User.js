3cd8d2871de12e18a7b3b74facd83e5a0ce2e0b5
nullgtx <chupunk4@gmail.com>
2024-10-19 00:31:18 +0700

    #1-update-user-model

--- models/User.js
+++ models/User.js
@@ -1,21 +1,40 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/config');
+'use strict';
+const { Model } = require('sequelize');
 
-const User = sequelize.define('User', {
-    username: {
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here if needed (e.g., User.hasMany(models.Post))
+    }
+  }
+
+  User.init(
+    {
+      username: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
-    },
-    password: {
+      },
+      password: {
         type: DataTypes.STRING,
         allowNull: false,
-    },
-    email: {
+      },
+      email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+      },
     },
-});
+    {
+      sequelize,
+      modelName: 'User', // Model name in Sequelize
+    }
+  );
 
-module.exports = User;
+  return User;
+};

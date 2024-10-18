8ec11abc4b95f24ed46a1fcbf8910324d83de313
nullgtx <chupunk4@gmail.com>
2024-10-19 00:29:13 +0700

    #1-add-config-js-connect-env

--- config/config.js
+++ config/config.js
@@ -1,9 +1,25 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from .env file
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
-});
-
-module.exports = sequelize;
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST, // you might want to use a different test DB
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_PRODUCTION, // you might want to use a different production DB
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+};

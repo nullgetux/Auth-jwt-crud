2e48839c7df8a8e08cfba12300fda34ef2ed01f0
nullgetux <chupunk4@gmail.com>
2024-10-19 16:06:08 +0700

    #1-change-username-to-nama

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,12 +8,12 @@ const saltRounds = 10;
 const authController = {
     // Register
     register: async (req, res) => {
-        const { username, password, email } = req.body;
+        const { nama, password, email } = req.body;
         try {
             const hashedPassword = await bcrypt.hash(password, saltRounds);
             const user = await User.create(
                 { 
-                    username, 
+                    nama, 
                     password: hashedPassword, 
                     email 
                 });
@@ -32,11 +32,11 @@ const authController = {
 
     // Login
     login: async (req, res) => {
-        const { username, password } = req.body;
+        const { email, password } = req.body;
         try {
             const user = await User.findOne(
                 { 
-                    where: { username } 
+                    where: { email } 
                 });
             if (!user) {
                 return res.status(401).json(
@@ -51,7 +51,7 @@ const authController = {
                         error: 'Invalid credentials' 
                     });
             }
-            const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
             res.status(200).json(
                 { 
                     message: 'Login successful', 

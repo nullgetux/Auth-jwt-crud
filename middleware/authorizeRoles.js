bfbdd644a618e069950b91a78338c52627f38df9
nullgetux <chupunk4@gmail.com>
2024-10-19 22:59:37 +0700

    #1-verify-role-user

--- middleware/authorizeRoles.js
+++ middleware/authorizeRoles.js
@@ -1,7 +1,8 @@
 // Middleware untuk memeriksa role user
 const authorizeRoles = (...roles) => {
     return (req, res, next) => {
-      if (!roles.includes(req.user.role)) {
+      if (!roles.includes(req.user.roleId)) {
+        console.log('Role ID:', req.user.roleId);
         return res.status(403).send('Access denied');
       }
       next();

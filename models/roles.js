b1c841c6a446aed879d7441cd963b1a9db91afc2
nullgetux <chupunk4@gmail.com>
2024-10-19 23:24:02 +0700

    #1-edit-relasi-user

--- models/roles.js
+++ models/roles.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Roles.hasMany(models.Users, { foreignKey: 'roleId', as: 'users' });
+      Roles.hasMany(models.Users, { foreignKey: 'roleId', as: 'user' });
     }
   }
   Roles.init({

bb3df97b3d4ff326d807bd9adfc12e957a850040
nullgetux <chupunk4@gmail.com>
2024-10-19 15:38:45 +0700

    #1-add-migration-user

--- migrations/20241018172323-create-user.js
+++ migrations/20241018172323-create-user.js
@@ -16,7 +16,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       password: {
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,

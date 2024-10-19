9e6b710aad70a7529ab6689c08243e1580c30d88
nullgetux <chupunk4@gmail.com>
2024-10-19 23:23:19 +0700

    #1-add-relasi-roles-and-jwt-rolename-and-req.user

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,7 +35,10 @@ const authController = {
         const { email, password } = req.body;
         try {
             // Query the user based on email
-            const user = await Users.findOne({ where: { email } });
+            const user = await Users.findOne({ 
+                where: { email },
+                include: [{ model: Roles, attributes: ['nama'], as: 'role' }]
+             });
             if (!user) {
                 return res.status(401).json({ error: 'Invalid credentials' });
             }
@@ -46,10 +49,15 @@ const authController = {
             }
            // Generate JWT token
             const token = jwt.sign(
-                { id: user.id, email: user.email, roleId: user.roleId },
+                { id: user.id, email: user.email, roleName: user.role.nama },
                 process.env.JWT_SECRET,
                 { expiresIn: '1h' }
             );
+            req.user = {
+                id: user.id,
+                email: user.email,
+                roleName: user.role.name // Attach role name here
+            };
             return res.status(200).json({ message: 'Login successful', token });
         } catch (error) {
             return res.status(400).json({ error: 'Login failed', details: error.message });

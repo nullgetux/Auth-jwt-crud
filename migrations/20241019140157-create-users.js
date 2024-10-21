9202429503800f84eb4b436c36c6bb3ff914ed1b
nullgetux <chupunk4@gmail.com>
2024-10-21 12:43:34 +0700

    #1-create-email-not-null-and-and-unique

--- migrations/20241019140157-create-users.js
+++ migrations/20241019140157-create-users.js
@@ -13,7 +13,9 @@ module.exports = {
         type: Sequelize.STRING
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
       },
       password: {
         type: Sequelize.STRING

743585e260b49f6964e74d33551a33e910f8a6d4
nullgetux <chupunk4@gmail.com>
2024-10-22 15:58:43 +0700

    #1-return-token-from-cookies

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,15 +2,13 @@ const jwt = require('jsonwebtoken');
 
 // Middleware untuk memverifikasi JWT
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  
-  // Check if the authorization header exists and starts with 'Bearer '
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(403).send('Token not provided');
-  }
+ // Extract the token from the cookies
+ const token = req.cookies.token;
 
-  // Extract the token by removing 'Bearer ' prefix
-  const token = authHeader.split(' ')[1];
+ // Check if the token exists in the cookies
+ if (!token) {
+  return res.status(403).send('Token not provided');
+}
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {

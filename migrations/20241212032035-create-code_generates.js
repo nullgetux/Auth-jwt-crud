40236ff294681a641354dac75e69fa7ce65a7307
nullgetux <chupunk4@gmail.com>
2024-12-16 11:39:17 +0700

    #5-change-migration

--- migrations/20241212032035-create-code_generates.js
+++ migrations/20241212032035-create-code_generates.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('code_generates', {
+    await queryInterface.createTable('codeGenerates', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
@@ -33,6 +33,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('code_generate');
+    await queryInterface.dropTable('codeGenerates');
   },
 };
--- migrations/20241212032230-create-prefix_numbers.js
+++ migrations/20241212032230-create-prefix_numbers.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('prefix_numbers', {
+    await queryInterface.createTable('prefixNumbers', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
@@ -33,6 +33,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('prefix_number');
+    await queryInterface.dropTable('prefixNumbers');
   },
 };

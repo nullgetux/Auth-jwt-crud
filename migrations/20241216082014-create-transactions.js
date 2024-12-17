b483dc142bd2ffb34827cff940dafa38bd0d7a05
nullgetux <chupunk4@gmail.com>
2024-12-17 14:55:16 +0700

    #5-edit-field-type-to-category

--- migrations/20241216082014-create-transactions.js
+++ migrations/20241216082014-create-transactions.js
@@ -24,7 +24,7 @@ module.exports = {
         allowNull: true,
         unique: true,
       },
-      transaction_type: {
+      transaction_category: {
         type: Sequelize.STRING,
         allowNull: true,
       },
